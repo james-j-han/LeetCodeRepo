2925fc38dd1808702d52fb1f37ff72748f065b56
James Han <42987815+james-j-han@users.noreply.github.com>
2024-12-07 23:12:15 +0000

    643 Maximum Average Subarray 1 Solution 12/7/2024

--- easy/643_maximum_average_subarray_1.py
+++ easy/643_maximum_average_subarray_1.py
@@ -1,4 +1,5 @@
 class Solution(object):
+
     def findMaxAverage(self, nums, k):
         """
         :type nums: List[int]
@@ -7,8 +8,40 @@ class Solution(object):
         """
         curr = ans = float(sum(nums[:k]))
         for i in range(k, len(nums)):
-            curr += nums[i] - nums[i-k] # sliding window
+            curr += nums[i] - nums[i - k]  # sliding window
             if curr > ans:
                 ans = curr
 
         return ans / k
+
+
+# 12/7/2024
+class Solution:
+
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        # ans var
+        # curr var
+        # left = 0
+
+        # for num in nums[:k-1]
+        # curr += num
+        # ans = curr / k
+
+        # for right in range(len(nums)):
+        # add right to curr
+        # sub left from curr
+        # ans = max(ans, curr / k)
+
+        # return ans
+        left = curr = 0
+        for num in nums[:k]:
+            curr += num
+
+        ans = curr / k
+
+        for num in nums[k:]:
+            curr += num - nums[left]
+            left += 1
+            ans = max(ans, curr / k)
+
+        return ans
